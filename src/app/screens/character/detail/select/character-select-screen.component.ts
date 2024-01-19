522292f987cd8d0d4c45f002c06ca8eccceef3d3
faileon <pt4cekt@gmail.com>
2024-01-19 16:51:07 +0100

    character grid per game

--- src/app/screens/character/detail/select/character-select-screen.component.ts
+++ src/app/screens/character/detail/select/character-select-screen.component.ts
@@ -1,26 +1,22 @@
-import {Component, OnInit, SkipSelf} from '@angular/core';
-import {CharacterService} from '../../../../services/character.service';
-import {Character} from '../../../../types';
-import {Observable} from 'rxjs';
+import { Component, OnInit, SkipSelf } from '@angular/core';
+import { CharacterService } from '../../../../services/character.service';
+import { Character } from '../../../../types';
+import { Observable } from 'rxjs';
+import { GameService } from '../../../../services/game.service';
 
 @Component({
   selector: 'tg-character-select',
   templateUrl: './character-select-screen.component.html',
-  styleUrls: ['./character-select-screen.component.scss']
+  styleUrls: ['./character-select-screen.component.scss'],
 })
-export class CharacterSelectScreenComponent implements OnInit {
-
+export class CharacterSelectScreenComponent {
   public characters$: Observable<Character[]>;
+  public selectedGame = this.gameService.selectedGame$;
 
-  constructor(@SkipSelf() public characterService: CharacterService) {
+  constructor(
+    @SkipSelf() public characterService: CharacterService,
+    private gameService: GameService,
+  ) {
     this.characters$ = this.characterService.getCharacters();
   }
-
-  ngOnInit(): void {
-  }
-
-  public onCharacterSelected(character: Character): void {
-    // console.log('selected', character);
-  }
-
 }
